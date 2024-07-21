a723bd97f04c9531c81a5748f0f7b0f2eafc5580
nikita <nikitaevtefeev@gmail.com>
2024-07-21 10:59:23 +0300

    fix multiple clients support

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import threading
 import time
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 from charcters import RandomCharacterGenerator
 import actions
@@ -8,16 +8,12 @@ import actions
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-hero = RandomCharacterGenerator.generate_random_character()
-opponentHero = RandomCharacterGenerator.generate_random_character()
+clients = {}
 
 
+def get_session_info():
+    return clients[request.sid]
 
-
-game = actions.Game()
-
-# Lock for thread-safe game state updates
-game_state_lock = threading.Lock()
 
 @app.route('/')
 def index():
@@ -26,33 +22,43 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
+    cid = request.sid
+    print("Connected:", cid)
+    clients[cid] = (cid, actions.Game(), threading.Lock())
+    id, game, game_state_lock = get_session_info()
     with game_state_lock:
         emit('gameState', game.getState())
+
 
 @socketio.on('respawn')
 def handle_connect():
+    cid, game, game_state_lock = get_session_info()
+
     with game_state_lock:
         game.respawn()
-        emit('gameState', game.getState())
+        emit('gameState', game.getState(), to=cid)
+
 
 @socketio.on('playCard')
 def handle_play_card(card):
+    cid, game, game_state_lock = get_session_info()
+
     with game_state_lock:
         damage, message, state = game.attackOpponent()
-        emit('heroAttackDamage', (damage, message), broadcast=True)
-        emit('gameState', game.getState(), broadcast=True)
-        
+        emit('heroAttackDamage', (damage, message), to=cid)
+        emit('gameState', game.getState(), to=cid)
+
         if damage == 0 or state == "killed":
             return
-            
+
         em_id = id(game.opponentHero)
     time.sleep(1)
     with game_state_lock:
         if em_id == id(game.opponentHero):
             damage, message = game.attackHero()
             if damage > 0:
-                emit('opponentAttackDamage', (damage, message), broadcast=True)
-                emit('gameState', game.getState(), broadcast=True)
+                emit('opponentAttackDamage', (damage, message), to=cid)
+                emit('gameState', game.getState(), to=cid)
 
 
 if __name__ == '__main__':
