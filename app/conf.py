6d07e4fd4b989a69148866672a360f8e55e22789
nikita <nikitaevtefeev@gmail.com>
2025-07-20 18:22:09 +0300

    fix redis conf

--- app/conf.py
+++ app/conf.py
@@ -1,7 +1,11 @@
 from app.storages import MemoryStorage, RedisStorage
 
 ENABLE_CHEAT = False
-STORAGE = MemoryStorage()
+# STORAGE = MemoryStorage()
 # STORAGE = RedisStorage()
 # USE_REDIS = False
 USE_REDIS = True
+if USE_REDIS:
+    STORAGE = RedisStorage()
+else:
+    STORAGE = MemoryStorage()
